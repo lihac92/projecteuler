be52e6b784b187739fcafcb3ab6443807bbf9c63
Alex <azubik@yanex.ru>
2019-12-26 22:59:54 +0300

    1_problem

---
+++ 1.py
@@ -0,0 +1,10 @@
+def sum_num(num):
+    counter = 1
+    result = 0
+    while counter < num:
+        if counter % 3 = 0 or counter % 5 = 0:
+            result = result + counter
+        counter++
+    return result
+
+print(sum_num(1000))
